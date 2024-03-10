9919533c1101363975c87fb3567b23a5bc4d2fa8
pradhanashis2k03 <100114532+pradhanashis2k03@users.noreply.github.com>
2024-03-10 05:39:19 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -7,7 +7,7 @@ MIT license
 import os
 
 if __name__ == "__main__":
-    cmd = "curl -o ./LA.zip -# https://datashare.ed.ac.uk/bitstream/handle/10283/3336/LA.zip\?sequence\=3\&isAllowed\=y"
+    cmd = "curl -o /content/gdrive/MyDrive/hoho/LA.zip -# https://datashare.ed.ac.uk/bitstream/handle/10283/3336/LA.zip\?sequence\=3\&isAllowed\=y"
     os.system(cmd)
-    cmd = "unzip LA.zip"
+    cmd = "unzip /content/gdrive/MyDrive/hoho/LA.zip"
     os.system(cmd)
